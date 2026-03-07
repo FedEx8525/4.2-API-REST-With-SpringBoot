c44b44dae646f445d005030a8ed40fa9e906a235
Federico Cantore <f.cantore85@gmail.com>
2026-03-07 13:27:01 +0100

    fix: take out save override

--- src/main/java/cat/itacademy/s04/t02/n01/fruit/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/repository/FruitRepository.java
@@ -1,11 +1,8 @@
 package cat.itacademy.s04.t02.n01.fruit.repository;
 
 import cat.itacademy.s04.t02.n01.fruit.model.Fruit;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FruitRepository extends CrudRepository<Fruit, Long> {
 
-    @Override
-    Fruit save(Fruit fruit);
 }
