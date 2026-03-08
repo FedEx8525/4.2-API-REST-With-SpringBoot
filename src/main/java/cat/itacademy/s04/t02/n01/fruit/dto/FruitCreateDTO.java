77ad6adabe66a6178b88aed29d64b76d43708ef5
Federico Cantore <f.cantore85@gmail.com>
2026-03-08 13:45:52 +0100

    feat: add createFruit_returnsFruitWithId

--- src/main/java/cat/itacademy/s04/t02/n01/fruit/dto/FruitCreateDTO.java
+++
@@ -1,4 +0,0 @@
-package cat.itacademy.s04.t02.n01.fruit.dto;
-
-public record FruitCreateDTO(String name, int weightKilos) {
-}
--- src/test/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitControllerTest.java
+++ src/test/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitControllerTest.java
@@ -1,4 +1,49 @@
 package cat.itacademy.s04.t02.n01.fruit.controllers;
 
+import cat.itacademy.s04.t02.n01.fruit.model.Fruit;
+import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitCreateDTO;
+import cat.itacademy.s04.t02.n01.fruit.repository.FruitRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class FruitControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private FruitRepository fruitRepository;
+
+    @BeforeEach
+    void setup() {
+        fruitRepository.deleteAll();
+    }
+
+    @Test
+    void createFruit_returnsFruitWithId() throws Exception {
+        FruitCreateDTO createDTO = new FruitCreateDTO("orange", 50);
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("orange"))
+                .andExpect(jsonPath("$.weightInKilos").value(50));
+
+    }
 }
