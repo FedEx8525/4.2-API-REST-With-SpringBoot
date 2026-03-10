0197064727f1872849d68de0047e95fd28afb1c8
Federico Cantore <f.cantore85@gmail.com>
2026-03-10 12:28:58 +0100

    feat: added integration test createFruit_ShouldReturn201Created()

--- src/test/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitControllerTest.java
+++ src/test/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitControllerTest.java
@@ -1,46 +1,45 @@
 package cat.itacademy.s04.t02.n01.fruit.controllers;
 
-import cat.itacademy.s04.t02.n01.fruit.model.Fruit;
 import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitCreateDTO;
-import cat.itacademy.s04.t02.n01.fruit.repository.FruitRepository;
+import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitDTO;
+import cat.itacademy.s04.t02.n01.fruit.services.FruitService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(FruitController.class)
 public class FruitControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private FruitService fruitService;
+
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private FruitRepository fruitRepository;
-
-    @BeforeEach
-    void setup() {
-        fruitRepository.deleteAll();
-    }
-
     @Test
-    void createFruit_returnsFruitWithId() throws Exception {
-        FruitCreateDTO createDTO = new FruitCreateDTO("orange", 50);
+    void createFruit_ShouldReturn201Created() throws Exception {
+        FruitCreateDTO input = new FruitCreateDTO("orange", 50);
+
+        FruitDTO output = new FruitDTO(1L, "orange", 50);
+
+        when(fruitService.createFruit(any(FruitCreateDTO.class))).thenReturn(output);
 
         mockMvc.perform(post("/fruits")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createDTO)))
+                        .content(objectMapper.writeValueAsString(input)))
                 .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.name").value("orange"))
                 .andExpect(jsonPath("$.weightInKilos").value(50));
