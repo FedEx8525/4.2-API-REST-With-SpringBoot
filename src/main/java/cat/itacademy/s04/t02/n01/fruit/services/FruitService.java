19980671aeb90a6ada38bd3feaaedf1324243e6e
Federico Cantore <f.cantore85@gmail.com>
2026-03-10 13:48:12 +0100

    feat: add listFruits() method

--- src/main/java/cat/itacademy/s04/t02/n01/fruit/services/FruitService.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/services/FruitService.java
@@ -3,6 +3,9 @@ package cat.itacademy.s04.t02.n01.fruit.services;
 import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitCreateDTO;
 import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitDTO;
 
+import java.util.List;
+
 public interface FruitService {
     FruitDTO createFruit(FruitCreateDTO fruitCreateDTO);
+    List<FruitDTO> listFruits(String name);
 }
