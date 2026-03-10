39591f4269f12bb81c4e70f23fdd38493539e489
Federico Cantore <f.cantore85@gmail.com>
2026-03-10 16:56:12 +0100

    fix: change annotation @Validated to @Valid in createFruit

--- src/main/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitController.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitController.java
@@ -3,6 +3,7 @@ package cat.itacademy.s04.t02.n01.fruit.controllers;
 import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitRequestDTO;
 import cat.itacademy.s04.t02.n01.fruit.model.dto.FruitResponseDTO;
 import cat.itacademy.s04.t02.n01.fruit.services.FruitService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -22,7 +23,7 @@ public class FruitController {
     }
 
     @PostMapping
-    public ResponseEntity<FruitResponseDTO> createFruit(@Validated @RequestBody FruitRequestDTO fruitRequestDTO) {
+    public ResponseEntity<FruitResponseDTO> createFruit(@Valid @RequestBody FruitRequestDTO fruitRequestDTO) {
 
         FruitResponseDTO savedFruit = fruitService.createFruit(fruitRequestDTO);
 
