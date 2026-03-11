d8d3e0ff63a07e79f4b1b967ffd1aca24628c8d1
Federico Cantore <f.cantore85@gmail.com>
2026-03-11 17:08:18 +0100

    feat: add handleValidation handler for message 400 BAD_REQUEST

--- src/main/java/cat/itacademy/s04/t02/n01/fruit/exception/GlobalExceptionHandler.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/exception/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package cat.itacademy.s04.t02.n01.fruit.exception;
 import cat.itacademy.s04.t02.n01.fruit.exception.dto.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +17,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(ErrorResponseDTO.of(404, "NotFound", exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleValidation(MethodArgumentNotValidException exception) {
+        String message = exception.getBindingResult().getFieldErrors().stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponseDTO.of(400, "Bad Request" , message));
     }
 
 
